d9fc3b9057f185dc1a8f35618001dd2c993e7c79
mconalf2108 <mconalf2108@g.educaand.es>
2026-01-20 18:26:03 +0100

    practicas sabado

--- src/Ejercicio11/Lista.java
+++ src/Ejercicio11/Lista.java
@@ -1,17 +1,67 @@
 package Ejercicio11;
 import java.util.Arrays;
 public class Lista {
-    Integer tabla [];
+    private Integer tabla [];
 
-    public Lista(Integer[] tabla) {
+
+
+    public Lista() {
         this.tabla = new Integer[0];
     }
     public Integer getElementosInsertados(){
-    return this.tabla.length;
+        return tabla.length;
     }
+    public Integer getElementoPorIndice(int indice){
+        return this.tabla[indice];
+    }
+    public Integer buscarNumero(int num){
+        boolean encontrado=false;
+        int i=0;
+        while (i<tabla.length && !encontrado){
+             if(this.tabla[i]==num){
+                 encontrado=true;
+             }else{
+                 i++;
+             }
+
+        }
+        if (encontrado) {
+            return i;
+        } else {
+            return -1;
+        }
+
+    }
+
     public void insertarAlFinal(Integer num){
 
-         (this.tabla[tabla.length] - 1)= num ;
-
+       Integer [] temporal=Arrays.copyOf(tabla, tabla.length+1) ;
+        temporal[tabla.length] = num;
+         this.tabla = temporal;
     }
+    public void insertarAlPrincipio(Integer num){
+        Integer [] temporal = new Integer[tabla.length+1];
+        System.arraycopy(this.tabla,0,temporal,1,this.tabla.length);
+        //this tabla.length al final lo que hace es evitar el error de espacio en el array, ya que empezamos
+        // por uno y la cantidad es del anterior array;
+        temporal[0] = num;
+        this.tabla = temporal;
+        }
+    public void insertarNumeroPorIndice(int indice, int num){
+        if (indice>=0 && indice<=tabla.length) {
+            Integer[] temporal = new Integer[tabla.length + 1];
+            /**for (int i = 0; i < indice; i++) {
+             temporal[i] = this.tabla[i];
+             }esto está bien hecho, pero no hacia falta, podemos hacer esto:**/
+            System.arraycopy(this.tabla, 0, temporal, 0,indice);
+
+
+            System.arraycopy(this.tabla, indice, temporal, indice + 1, (this.tabla.length - indice));
+            temporal[indice] = num;
+            this.tabla = temporal;
+        }
+    }
+
+
+
 }
