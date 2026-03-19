176417a2b72bdea96091a3dd38617c0b7a20e337
Mauro Contador Alfonso <mconalf2108@g.educaand.es>
2026-03-19 12:47:03 +0100

    clase

---
+++ src/PRACTICA_MINECRAFT/Minecraft.java
@@ -0,0 +1,33 @@
+package PRACTICA_MINECRAFT;
+
+import java.util.Arrays;
+
+public class Minecraft {
+    Materiales[] inventario = new Materiales[10];
+
+
+    public void anadirMaterial(Materiales nuevoMaterial) {
+        for (Materiales inventario : this.inventario) {
+            if (inventario == null ) {
+                inventario = nuevoMaterial;
+                System.out.println("Añadido correctamente");
+            }else {
+                System.out.println("Error, el inventario esta lleno");
+            }
+        }
+    }
+    public void borrarMaterialSinMasa(){
+        int elementosAMover = 0;
+        for (int i = 0; i < inventario.length; i++) {
+            if(this.inventario[i] != null && this.inventario[i].getMasa() <= 0){
+                elementosAMover =  this.inventario.length - 1 - i;
+            }
+            if(elementosAMover > 0){
+                System.arraycopy(this.inventario,i + 1 ,this.inventario,i,elementosAMover);
+            }
+
+        }
+
+    }
+
+}
