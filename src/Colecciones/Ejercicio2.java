93a0b9509b12ed7b258c2926c64c08eff189f729
mconalf2108 <mconalf2108@g.educaand.es>
2026-04-08 17:39:29 +0200

    ejercicios preexamen(GOAT)

---
+++ src/Colecciones/Ejercicio2.java
@@ -0,0 +1,22 @@
+package Colecciones;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            int aleatorio = (int)(Math.random() * 10) + 1;
+            lista.add(aleatorio);
+        }
+        ArrayList<Integer> lista2 = new ArrayList<>();
+        for (int i = 0; i < lista.size(); i++) {
+            // 1. Sacamos el número actual de la lista original
+            int numeroActual = lista.get(i);
+            if (!lista2.contains(numeroActual)) {
+                lista2.add(numeroActual);
+            }
+        }
+    }
+}
